c3f86691658928cd7f588d13be593afed15d8f20
Szymon Wojciechowski <dev@wojciechowski.sh>
2025-03-24 12:18:42 +0100

    publish sns message from lambda

--- lambda/rule.py
+++ lambda/rule.py
@@ -1,5 +1,7 @@
 import boto3
 import datetime
+import json
+import os
 
 def lambda_handler(event, context):
     config_client = boto3.client('config')
@@ -8,7 +10,9 @@ def lambda_handler(event, context):
     serial_console_access_enabled = ec2_client.get_serial_console_access_status()['SerialConsoleAccessEnabled']
     
     compliance_type = 'NON_COMPLIANT' if serial_console_access_enabled else 'COMPLIANT'
-
+    
+    print(compliance_type)
+    
     response = config_client.put_evaluations(
         Evaluations=[
             {
@@ -21,4 +25,29 @@ def lambda_handler(event, context):
         ResultToken=event['resultToken']
     )
 
-    print(response)
+    if compliance_type == 'NON_COMPLIANT':
+        sns_client = boto3.client('sns')
+
+        sns_topic_arn = os.environ.get('SNS_TOPIC_ARN')
+
+        message = (
+            "EC2 Serial Console Access Compliance Alert\n\n"
+            f"EC2 serial console enabled in AWS account (ID: {event['accountId']}).\n\n"
+            "Details:\n"
+            f"Account ID: {event['accountId']}\n"
+            "Compliance Status: NON_COMPLIANT\n"
+            f"Timestamp: {datetime.datetime.now().isoformat()}\n\n"
+            "Use AWS config remediation to disable it."
+        )
+
+        print(message)
+    
+        sns_response = sns_client.publish(
+            TopicArn=sns_topic_arn,
+            Message=message,
+            Subject='EC2 Serial Console Access Compliance Alert'
+        )
+
+        print(f"SNS notification sent: {sns_response}")
+
+    return { 'complianceType': compliance_type }
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-cd templates
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <STACK_NAME> <BUCKET_NAME> <EMAIL_ADDRESS>"
+    exit 1
+fi
 
-STACK_NAME="DisableSerialConsole"
+STACK_NAME="$1"
+BUCKET_NAME="$2"
+EMAIL_ADDRESS="$3"
+
+cd templates
 
 aws cloudformation deploy \
     --template-file sns.yaml \
     --stack-name "$STACK_NAME-sns" \
+    --parameter-overrides EmailAddress="$EMAIL_ADDRESS" \
     --capabilities CAPABILITY_IAM \
     --region eu-west-1
 
 aws cloudformation deploy \
     --template-file lambda.yaml \
     --stack-name "$STACK_NAME-lambda" \
+    --parameter-overrides SNSStackName="$STACK_NAME-sns" S3BucketName="$BUCKET_NAME" \
     --capabilities CAPABILITY_IAM \
     --region eu-west-1
 
@@ -25,5 +34,6 @@ aws cloudformation deploy \
 aws cloudformation deploy \
     --template-file config.yaml \
     --stack-name "$STACK_NAME-config" \
+    --parameter-overrides LambdaStackName="$STACK_NAME-lambda" SSMStackName="$STACK_NAME-ssm" \
     --capabilities CAPABILITY_IAM \
     --region eu-west-1
